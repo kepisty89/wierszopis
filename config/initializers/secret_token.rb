e383b8cdc91e55b40e16e588ff65dbfdf5d0b5a1
Artur Rybak <artur.rybak@gmail.com>
2010-12-25 22:16:14 +0100

    Startujemy od czystej jak łza aplikacji railsowej

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wierszopis::Application.config.secret_token = '5e11800e24cfdb57599ff5acf10275e9a3965797f35d06631e17024a0bdcf4e8f37d8ea80a653750c80b4b06f268fb3d16c4c76be9e52f702ac04d38c85f3d9b'
