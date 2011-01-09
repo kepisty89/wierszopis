1613ff90728f6949b0ebfc60b0287443ba824858
Artur Rybak <artur.rybak@gmail.com>
2011-01-09 16:02:14 +0100

    Początki user_panelu i gravatary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,9 @@ module ApplicationHelper
     end
   end
 
+  def avatar_url(user)
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=80&d=#{CGI.escape(default_url)}"
+  end
 end
